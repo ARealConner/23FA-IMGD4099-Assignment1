098590e5b560e0190b9adc75bf59b78ae023c555
ARealConner <81334211+ARealConner@users.noreply.github.com>
2023-09-12 22:16:57 -0400

    initial upload: Add .gitignore and initial code for audio.js, index.html, main.css, main.js, and seagulls.js
    - Added .gitignore file to ignore specific files and directories
    - Added audio.js with initial implementation of Audio object
    - Added index.html file with basic HTML structure
    - Created main.css file with CSS styles for the webpage
    - Implemented main.js with loading shader, initializing seagulls, starting audio on body click, setting initial uniforms, updating frame count on each frame, rendering setup, and event listeners for mouse move and window resize
    - Added seagulls.js with constants and functions for setting up canvas, creating textures and buffers

---
+++ main.js
@@ -0,0 +1,48 @@
+import { default as seagulls } from './seagulls.js';
+// import { default as Video } from './video.js'
+import { default as Audio } from './audio.js';
+
+async function loadShader() {
+    const response = await fetch('shader.wgsl');
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.text();
+}
+
+async function main() {
+    // Initialize seagulls and load the shader
+    const sg = await seagulls.init();
+    const shader = await loadShader();
+
+    // Start audio on body click
+    document.body.onclick = Audio.start;
+
+    // Initial uniforms setting
+    let framecounter = 0;
+    sg.uniforms({
+        frame: framecounter,
+        res: [window.innerWidth, window.innerHeight],
+        audio: [0, 0, 0],
+        mouse: [0, 0, 0],
+    });
+
+    // Update frame count on each frame
+    sg.onframe(() => {
+        sg.uniforms.frame = framecounter++;
+    });
+
+    // Render setup
+    sg.render(shader, { uniforms: ['frame', 'res', 'audio', 'mouse'] }).run();
+
+    // Event listeners for mouse move and window resize
+    document.addEventListener('mousemove', (event) => {
+        sg.uniforms.mouse = [event.clientX, event.clientY, 0];
+    });
+
+    window.addEventListener('resize', () => {
+        sg.uniforms.res = [window.innerWidth, window.innerHeight];
+    });
+}
+
+main();
---
+++ shader.wgsl
@@ -0,0 +1,26 @@
+@group(0) @binding(0) var<uniform> frame: f32;
+@group(0) @binding(1) var<uniform> res:   vec2f;
+@group(0) @binding(2) var<uniform> audio:  vec3f;
+@group(0) @binding(3) var<uniform> mouse: vec3f;
+
+@vertex
+fn vs( @location(0) input : vec2f ) ->  @builtin(position) vec4f {
+  return vec4f( input, 0., 1.);
+}
+
+@fragment
+fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
+  // create normalized position coordinates in range 0-1
+  var p : vec2f = -1. + (pos.xy / res) * 2.;
+  var color: vec4f = vec4f(0.);
+  let nMouse = mouse.xy / res;
+  let frequency = 2.;
+  let gain = 1.;
+  let thickness = nMouse.y/20;
+  for( var i:f32 = 0.; i < nMouse.x*10; i+= 1. ) {
+    p.x += sin( p.y + (frame/60.) * i ) * gain;
+    color += abs( thickness / p.x );
+  }
+
+  return color;
+}
